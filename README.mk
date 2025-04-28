f365ef4c6741adc244c10ea9477287c7c66b8098
Jhonatan Parada <119643678+jhonatanparada499@users.noreply.github.com>
2025-04-28 18:02:40 -0400

    Create README.mk

---
+++ README.mk
@@ -0,0 +1,13 @@
+# Lab for Switching Configuration and VLAN Implementation
+
+This project was finished as a requirement for Queensborough Community College's (CUNY) Networking Fundamentals II course.
+
+## Summary
+The lab's main objectives were to configure network switches using the Cisco IOS CLI, create VLANs, secure ports, and confirm Layer 2 and Layer 3 communication between linked devices.
+
+## Important Tasks
+- Created, named, assigned ports to, and deleted VLANs on a Cisco 3550 switch.
+IP addresses were assigned and Switch Virtual Interfaces (SVIs) were deployed for remote access and management.
+By assigning particular MAC addresses to switch ports, port security was put into place.
+To maximize network performance, manually configure duplex modes and port speeds.
+Ping and IP configuration verification were used to test the connectivity between workstations assigned to VLANs.
